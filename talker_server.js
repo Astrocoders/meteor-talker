380e5808ade9a68bd75f83478daff4d11e03410a
Gabriel Rubens <grsabreu@gmail.com>
2016-01-05 12:08:25 -0200

    Suppress initial added hook firing

--- talker_server.js
+++ talker_server.js
@@ -3,7 +3,6 @@ Meteor.talks = function(name, fn){
   Meteor.publish(`astrocoders_talker/${name}`, function(){
     const talkerId = Random.id();
     const pub = this;
-    let boundTalkChange = talkChange.bind(null, {});
     let talker = fn( talkChange ) || {};
 
     this.added(TALKER_COLL, talkerId, {
@@ -12,18 +11,27 @@ Meteor.talks = function(name, fn){
     });
 
     if(talker._publishCursor){
-      console.log('Talker is a Mongo cursor, listening to its hooks');
+      // There's no direct way to suppress the initial fire of .added hook
+      let suppressInitial = true;
+      let boundTalkChange = () => {
+        if(!suppressInitial) talkChange();
+      };
+
       let observeHandler = talker.observeChanges({
         added: boundTalkChange,
         changed: boundTalkChange,
         remove: boundTalkChange,
+        movedBefore: boundTalkChange,
       });
+
+      suppressInitial = false;
 
       this.onStop(() => observeHandler.stop());
     }
 
     function talkChange(data = {}){
       check({data}, {data: Object});
+      console.log('[astrocoders:talker]', `Talking to ${name}`);
       pub.changed(TALKER_COLL, talkerId, _.extend({
         changingProp: Random.id(),
       }, data));
